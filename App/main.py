410eb7a7fa560add81422a327dcc1443927f04a4
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-05-30 15:48:44 +0100

    feat: cleaning + re add styling

--- App/main.py
+++ App/main.py
@@ -1,12 +1,10 @@
-import sys
-
 import qdarkstyle
-from PySide6.QtWidgets import QApplication
 from price_predictor import PricePredictor
+from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication([])
-    # app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyside6"))
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyside6"))
     window = PricePredictor()
     window.show()
     app.exec()
--- App/price_predictor.py
+++ App/price_predictor.py
@@ -1,13 +1,12 @@
 import json
+
 import joblib
 import numpy as np
-
 import pandas as pd
 import xgboost as xgb
 from price_predictor_ui import Ui_PricePredictor
 from PySide6.QtGui import QDoubleValidator
 from PySide6.QtWidgets import QMainWindow
-from sklearn.preprocessing import OneHotEncoder
 
 
 class PricePredictor(QMainWindow, Ui_PricePredictor):
@@ -21,7 +20,7 @@ class PricePredictor(QMainWindow, Ui_PricePredictor):
 
     def setup(self):
         self.model = xgb.Booster()
-        
+
         self.model.load_model("./Machine Learning/model.xgb")
         self.setupUi(self)
         double_validator = QDoubleValidator()
@@ -110,23 +109,22 @@ class PricePredictor(QMainWindow, Ui_PricePredictor):
         }
 
         df = pd.DataFrame(data, index=[0])
-        cat=[ 'body_type', 'fuel', 'transmission']
-        features=['brand', 'model', 'location', 'color']
-        les={}
+        cat = ["body_type", "fuel", "transmission"]
+        features = ["brand", "model", "location", "color"]
+        les = {}
         for f in features:
-            les[f]= joblib.load(f"./Machine Learning/{f}_encoder.pkl")
-        
+            les[f] = joblib.load(f"./Machine Learning/{f}_encoder.pkl")
+
         for f in features:
-            df[f]=les[f].transform(df[f])
+            df[f] = les[f].transform(df[f])
 
         encoder = joblib.load("./Machine Learning/encoder.pkl")
         OH_X = pd.DataFrame(encoder.transform(df[cat]))
         OH_X.index = df[cat].index
         df = df.drop(cat, axis=1)
 
-
-        new_data=pd.concat([df,OH_X],axis=1)
+        new_data = pd.concat([df, OH_X], axis=1)
         for f in features:
-            new_data[f]=new_data[f].astype(np.int0)
+            new_data[f] = new_data[f].astype(np.int0)
         print(new_data)
         return new_data
